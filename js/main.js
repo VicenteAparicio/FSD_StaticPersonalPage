3eef0d5536833041d7a52fa5cecbfe72371dd146
Goodbyez <miradadesigual@gmail.com>
2021-05-05 20:25:00 +0200

    Diseñando la landing page  por décima vez

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let home = document.getElementById('home');
+let contact = document.getElementById('contact');
+let portfolio = document.getElementById('portfolio');
+
+let textHome = ">Home";
+let textContact = ">Contact";
+let textPortfolio =">Portfolio";
+
+home.innerHTML=textHome;
+contact.innerHTML=textContact;
+portfolio.innerHTML=textPortfolio;
