961298b3cf43cbd80a305d53d4c46cd2f741fd22
Goodbyez <miradadesigual@gmail.com>
2021-05-05 20:50:29 +0200

    Quizá acercándonos a un diseño interesante

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ let home = document.getElementById('home');
 let contact = document.getElementById('contact');
 let portfolio = document.getElementById('portfolio');
 
-let textHome = ">Home";
-let textContact = ">Contact";
-let textPortfolio =">Portfolio";
+let textHome = ">home<";
+let textContact = "hontact";
+let textPortfolio ="portfolio";
 
 home.innerHTML=textHome;
 contact.innerHTML=textContact;
