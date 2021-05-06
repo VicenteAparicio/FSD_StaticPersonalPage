8d551ee95770fd2f77f4fe369c586b32f0502f5e
Goodbyez <miradadesigual@gmail.com>
2021-05-06 17:29:35 +0200

    Mejora del diseño responsive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 let home = document.getElementById('home');
+let home2 = document.getElementById('home2');
 let contact = document.getElementById('contact');
 let portfolio = document.getElementById('portfolio');
 
-let textHome = "/home";
+let textHome = "vicente";
+let textHome2 = "aparicio";
 let textContact = "contact";
 let textPortfolio ="portfolio";
 
 home.innerHTML=textHome;
+home2.innerHTML=textHome2;
 contact.innerHTML=textContact;
 portfolio.innerHTML=textPortfolio;
